516f0eb729483a99e3d376dddc7c7e3c0827d086
Tiqur <trevorbrage02@gmail.com>
2024-12-23 17:18:14 -0800

    Draw intitial screen

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
 
 
 const int DEBUG = false;
@@ -12,6 +13,21 @@ const int DEBUG = false;
 // This will be dynamic to video dimensions
 // Will also create black bars if window size doesn't conform
 const float ASPECT_RATIO = 0.5625;
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.0f, 0.0f, 0.00f, 1.0f);\n"
+"}\n\0";
 
 // Callback to handle window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -30,8 +46,20 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Initialize vertices
+    GLfloat vertices[] = 
+    {
+      0.0f, 0.0f, 0.0f,
+      0.0f, 0.0f, 0.0f,
+      0.0f, 0.0f, 0.0f,
+
+      0.0f, 0.0f, 0.0f,
+      0.0f, 0.0f, 0.0f,
+      0.0f, 0.0f, 0.0f,
+    };
+
     // Create a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Rewind", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Rewind", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -50,6 +78,63 @@ int main() {
 
     // Set the viewport
     glViewport(0, 0, 800, 600);
+
+    GLint success;
+    char infoLog[512];
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Vertex shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "Fragment shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Shader program linking failed:\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // Register the framebuffer size callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -72,12 +157,17 @@ int main() {
 
     // Main render loop
     while (!glfwWindowShouldClose(window)) {
-        int width, height;
+        int width, height, sidebarWidth;
         glfwGetWindowSize(window, &width, &height);
+        sidebarWidth = std::min(std::max((int)(width*0.2), 200), 400);
 
         // Render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/3);
 
         // New frame
         ImGui_ImplOpenGL3_NewFrame();
@@ -125,7 +215,7 @@ int main() {
 
         // Sidebar
         ImGui::SetNextWindowPos(ImVec2(0, toolbarHeight));
-        ImGui::SetNextWindowSize(ImVec2(std::min(std::max((int)(width*0.2), 200), 400), height-toolbarHeight));
+        ImGui::SetNextWindowSize(ImVec2(sidebarWidth, height-toolbarHeight));
         ImGui::Begin("ImGUI Window", &p_open, window_flags);
         ImGui::Text("Clips");
         ImGui::SeparatorText("Clips");
@@ -135,6 +225,25 @@ int main() {
         ImGui::End();
 
 
+        // Edit verticies
+        int scrubBarHeight = (int)std::min(std::max(height*0.25, 100.0), 200.0);
+        // Triangle 1
+        vertices[0] = (2.0*sidebarWidth)/width-1;
+        vertices[1] = 1.0;
+        vertices[3] = 1.0;
+        vertices[4] = -((2.0*toolbarHeight)/height-1);
+        vertices[6] = (2.0*sidebarWidth)/width-1;
+        vertices[7] = (2.0*scrubBarHeight)/height-1;
+
+        // Triangle 2
+        vertices[9]  = 1.0f;
+        vertices[10] = 1.0;
+        vertices[12] = 1.0;
+        vertices[13] = (2.0*scrubBarHeight)/height-1;
+        vertices[15] = (2.0*sidebarWidth)/width-1;
+        vertices[16] = (2.0*scrubBarHeight)/height-1;
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 
         // Renders the ImGUI elements
         ImGui::Render();
@@ -148,6 +257,10 @@ int main() {
     // Clean up and exit
     glfwDestroyWindow(window);
     glfwTerminate();
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     return 0;
 }
 
